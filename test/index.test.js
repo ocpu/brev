32ce9237a7419f7ce3d176f1041483b2881e88b9
Martin Hövre <martin.hovre@opencubes.io>
2018-01-17 11:42:02 +0100

    Should fix puppeteer not running

--- test/index.test.js
+++ test/index.test.js
@@ -196,7 +196,7 @@ it('observers can unregister themselves', () => {
 })
 
 it('evaluates in browser', async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] })
   const page = await browser.newPage()
   await page.addScriptTag({ path: './index.js' })
   expect(page.evaluate(() => {
